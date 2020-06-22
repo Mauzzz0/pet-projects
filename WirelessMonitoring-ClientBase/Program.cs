944335c154725fcce2ac00d4ae35bd4baf815514
Ruslan <ruslan.semack1501@gmail.com>
2020-06-22 21:47:40 +0300

    Init commit

---
+++ WirelessMonitoring-ClientBase/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace WirelessMonitoring_ClientBase
+{
+    class Program
+    {
+        // адрес и порт сервера, к которому будем подключаться
+        static int port = 8005; // порт сервера
+        static string address = "192.168.1.68"; // адрес сервера
+        static void Main(string[] args)
+        {
+            try
+            {
+                int i = 0;
+                //while (true)
+                //{
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // подключаемся к удаленному хосту
+                socket.Connect(ipPoint);
+                //Console.Write("Введите сообщение:");
+                //string message = Convert.ToString(i);
+                //byte[] data = Encoding.Unicode.GetBytes(message);
+                //socket.Send(data);
+                //i++;
+                //Thread.Sleep(2000);
+                //}
+                //// получаем ответ
+                while (true)
+                {
+                    byte[] data = new byte[256]; // буфер для ответа
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байт
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (socket.Available > 0);
+                    Console.WriteLine("ответ сервера: " + builder.ToString());
+                }
+                //
+                //// закрываем сокет
+                //socket.Shutdown(SocketShutdown.Both);
+                //socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
