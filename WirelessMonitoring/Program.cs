3455c8f8859783fec18783a5fde166693c0eeaf6
Ruslan <ruslan.semack1501@gmail.com>
2020-06-23 16:39:00 +0300

    Release 1.0

--- WirelessMonitoring/Program.cs
+++ WirelessMonitoring/Program.cs
@@ -16,9 +16,8 @@ namespace WirelessMonitoring
         const int server_port = 8005;
         const string server_ip = "192.168.1.68";
 
-        static string cpu_name, cpu_cores, cpu_threads, cpu_percent, pc_name, ram_total, ram_available;
-        static int screen_height, screen_width;
-        static double ram_percent;
+        static string cpu_name, cpu_cores, cpu_threads, cpu_percent, pc_name, ram_total, ram_available, ram_percent;
+        static string screen_height, screen_width;
         public delegate void UpdateTBCallback(int key,string text);
         static public void Start()
         {
@@ -47,9 +46,9 @@ namespace WirelessMonitoring
                     while (true)
                     {
                         GetInfo();
-                        string message1 = cpu_name+" "+ cpu_cores+ " " + cpu_threads + " " + pc_name + " " + ram_total + " " + ram_available +"\n"; /// Make list
-                        string message2= Convert.ToString(screen_height)+" "+Convert.ToString(screen_width)+"\n";
-                        string message3 = Convert.ToString(ram_percent) +" "+ cpu_percent+"\n";
+                        string message1 = cpu_name+","+ cpu_cores+ "," + cpu_threads + "," + pc_name + "," + ram_total + "," + ram_available +","; /// Make list
+                        string message2= screen_height+","+screen_width+",";
+                        string message3 = ram_percent +","+ cpu_percent;
                         byte[] data = Encoding.Unicode.GetBytes(message1+message2+message3);
                         handler.Send(data);
                         Thread.Sleep(2000);
@@ -85,15 +84,15 @@ namespace WirelessMonitoring
             {
                 ram_total = mo["TotalVirtualMemorySize"].ToString();
                 ram_available = mo["FreeVirtualMemory"].ToString();
-                ram_percent = 1-(Convert.ToDouble(ram_available) / Convert.ToDouble(ram_total));
+                ram_percent = Convert.ToString(1-(Convert.ToDouble(ram_available) / Convert.ToDouble(ram_total))).Replace(',','.');
             }
 
             query = new WqlObjectQuery("Select * from Win32_DesktopMonitor where DeviceID='DesktopMonitor1'");
             find = new ManagementObjectSearcher(query);
             foreach (ManagementObject mo in find.Get())
             {
-                screen_height = Convert.ToInt32(mo["ScreenHeight"]);
-                screen_width = Convert.ToInt32(mo["ScreenWidth"]);
+                screen_height = Convert.ToString(mo["ScreenHeight"]);
+                screen_width = Convert.ToString(mo["ScreenWidth"]);
             }
         }
     }
