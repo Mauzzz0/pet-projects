3a54c3143d3637c519bc484bcf3680cd3a49d793
Ruslan <ruslan.semack1501@gmail.com>
2020-06-23 16:48:48 +0300

    Add Xamarin-version Client

---
+++ MobileClient/MobileClient/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using static MobileClient.MainPage;
+
+namespace MobileClient
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        static internal MainPage instance { get; set; }
+        static bool isConnected = false;
+        public MainPage()
+        {
+            instance = this;
+            InitializeComponent();
+        }
+
+        private async void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            if (isConnected == false)
+            {
+                Program.Start();
+                //Thread thrd = new Thread(Program.Start);
+                //thrd.Start();
+                //label1.Text = "async method started";
+                //await Task.Delay(1000); // example purpose only
+                //label1.Text = "1 second passed";
+                //await Task.Delay(2000);
+                //label1.Text = "2 more seconds passed";
+                isConnected = true;
+            }
+        }
+
+        internal static void changeLabels(string[] infos)
+        {
+            string cpu_name = infos[0];
+            string cpu_cores = infos[1];
+            string cpu_threads = infos[2];
+            string pc_name = infos[3];
+            string ram_total = infos[4];
+            string ram_available = infos[5];
+            string screen_height = infos[6];
+            string screen_width = infos[7];
+            string ram_percent = infos[8];
+            string cpu_percent = infos[9];
+
+            instance.label1.Text =  ram_percent;
+            instance.Cpu_name.Text =  cpu_name;
+            instance.Cpu_cores.Text = cpu_cores;
+            instance.Cpu_threads.Text = cpu_threads;
+            instance.Cpu_percent.Text = cpu_percent;
+            instance.Pc_name.Text = pc_name;
+            instance.Ram_Available.Text = ram_available;
+            instance.Ram_total.Text = ram_total;
+            instance.Ram_percent.Text = ram_percent;
+            instance.Screen_height.Text = screen_height;
+            instance.Screen_width.Text = screen_width;
+        }
+    }
+}
---
+++ MobileClient/MobileClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using static MobileClient.MainPage;
+
+namespace MobileClient
+{
+    class Program
+    {
+        // адрес и порт сервера, к которому будем подключаться
+        static int port = 8005; // порт сервера
+        static string address = "192.168.1.68"; // адрес сервера
+        static string cpu_name, cpu_cores, cpu_threads, cpu_percent, pc_name, ram_total, ram_available, ram_percent;
+        static string screen_height, screen_width;
+        internal async static void Start()
+        {
+            //try
+            //{
+                int i = 0;
+                //while (true)
+                //{
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // подключаемся к удаленному хосту
+                socket.Connect(ipPoint);
+                //Console.Write("Введите сообщение:");
+                //string message = Convert.ToString(i);
+                //byte[] data = Encoding.Unicode.GetBytes(message);
+                //socket.Send(data);
+                //i++;
+                //Thread.Sleep(2000);
+                //}
+                //// получаем ответ
+                while (true)
+                {
+                    byte[] data = new byte[512]; // буфер для ответа
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байт
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        string info = builder.ToString();
+                        string[] infos = info.Split(',').ToArray();
+                        changeLabels(infos);
+                        await Task.Delay(2000);
+                    }
+                    while (socket.Available > 0);
+                }
+                //
+                //// закрываем сокет
+                //socket.Shutdown(SocketShutdown.Both);
+                //socket.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //}
+        }
+    }
+}
