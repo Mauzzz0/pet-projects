4e448a1164eeaa2194bcaae5606d8fe2bbdd7587
Mauzzz0 <ruslan.semack1501@gmail.com>
2020-12-07 17:34:55 +0300

    System Rollback

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.gesturesurface import GestureSurface
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.layout import Layout
